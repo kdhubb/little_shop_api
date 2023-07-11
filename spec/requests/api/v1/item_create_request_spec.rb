f6152cd716e19538356854c0ae93185228e525ae
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-11 16:09:17 -0600

    add test for extra data in create

--- spec/requests/api/v1/item_create_request_spec.rb
+++ spec/requests/api/v1/item_create_request_spec.rb
@@ -22,6 +22,28 @@ RSpec.describe "Create Item Request", type: :request do
       expect(new_item.unit_price).to eq(item_data[:unit_price])
       expect(new_item.merchant_id).to eq(item_data[:merchant_id])
     end
+
+    it "ignores disallowed attributes" do 
+      item_data = {
+        "name": "Dogpack",
+        "description": "Backpack for your dog to carry their own stuff.",
+        "rating": 5,
+        "materials": "ripstop nylon",
+        "unit_price": 5.00,
+        "merchant_id": @merchant.id
+      }
+
+      post "/api/v1/items", params: item_data
+      new_item = Item.last
+
+      expect(response.code).to eq("201")
+      expect(new_item.name).to eq(item_data[:name])
+      expect(new_item.description).to eq(item_data[:description])
+      expect(new_item.unit_price).to eq(item_data[:unit_price])
+      expect(new_item.merchant_id).to eq(item_data[:merchant_id])
+      expect(new_item.attributes[:materials]).to be nil
+      expect(new_item.attributes[:rating]).to be nil
+    end
     
     it "rejects invalid data" do 
       item_data = {
