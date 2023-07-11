ae9ca4b188fe03eb12cdc21da4c1d6340dccfd33
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-11 09:34:53 -0600

    add transaction associations and tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
   it { should have_many :invoice_items }
+  it { should have_many :transactions }
   it { should have_many(:items).through (:invoice_items)}
   it { should belong_to :customer }
   it { should belong_to :merchant }
