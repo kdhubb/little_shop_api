893bf747ce9013863f2fd5ef626e34733b16039d
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-10 21:24:26 -0600

    add show action and edit serializer

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::MerchantsController < ApplicationController 
   def index 
-    render json: Merchant.all
+    render json: MerchantSerializer.new(Merchant.all)
+    # render json: jsonapi_serialize(merchants)
+  end
+  
+  def show 
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 end
