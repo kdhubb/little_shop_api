abe375f3225bcc8c2862f960d5dd911629c18405
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-10 21:56:20 -0600

    add response test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,12 +1,19 @@
 require "rails_helper" 
 
-RSpec.describe "Merchants API" do 
-  it "sends a list of all merchants" do
-    get "/api/v1/merchants" 
+RSpec.describe "Merchants API", type: :request do 
+  describe "it sends a list of all merchants" do 
+    before(:each) do 
+      get "/api/v1/merchants" 
+      merchants = JSON.parse(response.body, symbolize_names: true)
+    end
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
-    expect(response).to be_successful
-    expect(merchants["data"].count).to eq(3)
-    expect(merchants).to include(:data)
+    it "has a successful request" do
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('{"data":[]}')
+    end
+
+    it "has keys" do 
+
+    end
   end
 end
