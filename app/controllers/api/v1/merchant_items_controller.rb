8e992edf99037938175e1c1b464fdd7749199960
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-12 15:56:24 -0600

    wip merchant find with string error

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::MerchantItemsController < ApplicationController
+
   def index 
-    render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+    render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items.errors)
+    render json: Merchant.find(params[:merchant_id]).errors, status: :not_found
   end
 end
