6ebaf444eec0ccc15ac5f815cd4a68f292d278e8
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-11 18:44:40 -0600

    add finished update item tests

--- spec/requests/api/v1/item_update_spec.rb
+++ spec/requests/api/v1/item_update_spec.rb
@@ -25,24 +25,40 @@ RSpec.describe "Item Update", type: :request do
     end
 
     it "with missing Merchant id" do 
+      item = create(:item)
+      previous_name = item.name
+      previous_description = item.description
+      previous_unit_price = item.unit_price
 
+      update_data = {
+          "name": "Dogpack",
+          "description": "Backpack for your dog to carry their own stuff.",
+          "unit_price": 15.00
+        }
+
+      patch "/api/v1/items/#{item.id}", params: update_data
+      updated_item = Item.find(item.id)
+
+      expect(response.code).to eq("200")
     end
 
     it "fails to update with invalid merchant id" do 
+      item = create(:item)
+      previous_name = item.name
+      previous_description = item.description
+      previous_unit_price = item.unit_price
 
+      update_data = {
+          "name": "Dogpack",
+          "description": "Backpack for your dog to carry their own stuff.",
+          "unit_price": 15.00,
+          "merchant_id": "2"
+        }
+
+      patch "/api/v1/items/#{item.id}", params: update_data
+      updated_item = Item.find(item.id)
+
+      expect(response.code).to eq("404")
     end
   end
 end
-
-{"name"=>"Shiny Itemy Item, New and Improved", 
-  "description"=>"It does a lot of things even more good than before!", 
-  "unit_price"=>65.23, 
-  "merchant_id"=>56, 
-  "controller"=>"api/v1/items", 
-  "action"=>"update", 
-  "id"=>"179", 
-  "item"=>{
-    "name"=>"Shiny Itemy Item, New and Improved", 
-    "description"=>"It does a lot of things even more good than before!", 
-    "unit_price"=>65.23, 
-    "merchant_id"=>56}}
