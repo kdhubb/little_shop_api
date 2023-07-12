240b47e8c65677bfc5008a3b31c7f00847bd7563
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-11 19:20:23 -0600

    formatting

--- spec/requests/api/v1/items/item_destroy_spec.rb
+++ spec/requests/api/v1/items/item_destroy_spec.rb
@@ -2,21 +2,16 @@ require "rails_helper"
 
 RSpec.describe "Destroy Item Request", type: :request do 
   describe "destroy an item" do 
-    it "with valid data" do 
+    it "destroys an item given the id" do 
       item = create(:item)
       
       expect(Item.count).to eq(1)
 
       delete "/api/v1/items/#{item.id}"
-
+      
       expect(response).to be_successful
       expect(Item.count).to eq(0)
       expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
-    end
-
-    it "with invalid data" do 
-      item = create(:item)
-
     end
   end
 end
