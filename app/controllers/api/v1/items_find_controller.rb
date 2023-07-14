7ed229c6724d841766d885e3f19e845da3e1f1ff
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-13 18:05:20 -0600

    add no paramaters given check - predicated  method

--- app/controllers/api/v1/items_find_controller.rb
+++ app/controllers/api/v1/items_find_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::ItemsFindController < ApplicationController
-  
+  before_action :verify_params
+
   def show
-    require 'pry'; binding.pry
     render json: ItemSerializer.new(Item.case_insensitive_search(params[:name]))
   end
 
   def index 
 
+  end
+
+  private 
+  def verify_params
+    if params[:name] == ""
+      render json: {error: "No search paramater given"}, status: :bad_request
+    end
   end
 end
