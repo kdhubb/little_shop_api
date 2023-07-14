b56b8c04b70c7832b971c0043a55613ea00d61e2
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-14 16:27:25 -0600

    add most_expensive method and transaction successful scope

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,4 +6,13 @@ class Invoice < ApplicationRecord
   has_many :transactions
 
   enum status: { cancelled: 0, active: 1 }
+
+  def self.most_expensive(limit = 5, sorting = "DESC")
+    select("invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .joins(:invoice_items, :transactions)
+    .merge(Transaction.successful)
+    .group(:id)
+    .order("revenue #{sorting}")
+    .limit(limit)
+  end
 end
